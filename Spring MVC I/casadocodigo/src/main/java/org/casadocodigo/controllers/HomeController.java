3011191cff471856bb9e8ba77b6516761a80560b
Vitor Rodrigo Vezani <vitor.vezani@gmail.com>
2017-02-12 22:23:49 -0200

    Refatoração das classes

---
+++ Spring MVC I/casadocodigo/src/main/java/org/casadocodigo/controllers/HomeController.java
@@ -0,0 +1,15 @@
+package org.casadocodigo.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+	@RequestMapping("/")
+	public String index() {
+		System.out.println("Entrando na home da CDC");
+		return "home";
+	}
+
+}
