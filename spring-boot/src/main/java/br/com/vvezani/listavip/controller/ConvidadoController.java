861fd7357e2683b00caabc68b93ef93e80a72b4f
Vitor Rodrigo Vezani <vitor.vezani@gmail.com>
2017-05-30 02:09:04 -0300

    Aula 2

---
+++ spring-boot/src/main/java/br/com/vvezani/listavip/controller/ConvidadoController.java
@@ -0,0 +1,29 @@
+package br.com.vvezani.listavip.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import br.com.vvezani.listavip.model.Convidado;
+import br.com.vvezani.listavip.repository.ConvidadoRepository;
+
+@Controller
+public class ConvidadoController {
+
+	@Autowired
+	private ConvidadoRepository repository;
+
+	@RequestMapping("/")
+	public String index() {
+		return "index";
+	}
+
+	@RequestMapping("listaconvidados")
+	public String listaConvidados(Model model) {
+		Iterable<Convidado> convidados = repository.findAll();
+		model.addAttribute("convidados", convidados);
+		return "listaconvidados";
+	}
+
+}
